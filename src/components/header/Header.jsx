779a9a0b0feb9b753a2e92ddefec1866d95fbe5f
antoniora97 <antoniora1997@gmail.com>
2022-12-28 13:36:21 +0100

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import CTA from './CTA'
+import './header.css'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Antonio Ramos</h1>
+        <h5 className='text-light'>Desarrollador WEB Fullstack</h5>
+        <CTA />
+
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
